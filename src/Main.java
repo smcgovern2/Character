5365c355153868755bc98770c97783b7437d3682
smcgovern2 <smcgovern2@my.wctc.edu>
2018-11-07 19:03:38 -0600

    Added starvation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Character basicCharacter = new Spear(new Sword(new BasicCharacter("Bilbo")));
+        Character basicCharacter = new Starving(new Shield(new Spear(new Sword(new BasicCharacter("Bilbo")))));
         System.out.println("Name: " + basicCharacter.getName());
         System.out.println("Heath: " + basicCharacter.getHealth());
     }
